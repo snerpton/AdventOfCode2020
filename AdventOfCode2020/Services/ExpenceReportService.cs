59fa5e6d940f507d9306301027da1d1dc8c73324
Chris Ashton <webdev@snerpton.net>
2020-12-01 21:05:46 +0000

    Add tests to ensure expense report is in a valid state to make calculation.

--- AdventOfCode2020/Services/ExpenceReportService.cs
+++ AdventOfCode2020/Services/ExpenceReportService.cs
@@ -11,12 +11,25 @@ namespace AdventOfCode2020.Services
         public ExpenceReportService(ExpenseReport expenceReport)
         {
             _expenceReport = expenceReport ?? throw new ArgumentNullException(nameof(expenceReport));
-        }
 
+            if (ValidateExpenceReport(_expenceReport) == false)
+                throw new ArgumentException(nameof(expenceReport));
+        }
 
         public int Find2NumbersThatAddUpTo2020AndMultiplyTogether()
         {
             throw new NotImplementedException();
+        }
+
+        private static bool ValidateExpenceReport(ExpenseReport expenceReport)
+        {
+            if (expenceReport.Entries == null)
+                return false;
+
+            if (expenceReport.Entries.Length < 2)
+                return false;
+
+            return true;
         }
     }
 }
--- AdventOfCode2020Tests/ExpenceReportServiceTest.cs
+++ AdventOfCode2020Tests/ExpenceReportServiceTest.cs
@@ -1,15 +1,36 @@
 using NUnit.Framework;
 using AdventOfCode2020.Services;
 using System;
+using AdventOfCode2020.Models;
 
 namespace AdventOfCode2020Tests
 {
     public class ExpenceReportServiceTest
     {
+        //private int[] exampleExpenseReportEntries = new int[] { 1721, 979, 366, 299, 675, 1456 };
+
         [Test]
         public void Should_ThrowException_When_NullExpenseReport()
         {
             Assert.Throws<ArgumentNullException>(() => new ExpenceReportService(null));
+        }
+
+        [Test]
+        public void Should_ThrowException_When_ExpenceReportEntriesAreNull()
+        {
+            var mockExpenceReport = new ExpenseReport() { Entries = null };
+
+            Assert.Throws<ArgumentException>(() => new ExpenceReportService(mockExpenceReport));
+        }
+
+        [Test]
+        [TestCase(new int[] { })]
+        [TestCase(new int[] { 1 })]
+        public void Should_ThrowException_When_ExpenceReportNumberOfEntriesIsEmptyOrLessThan2(int[] expenceEntries)
+        {
+            var mockExpenceReport = new ExpenseReport() { Entries = expenceEntries };
+
+            Assert.Throws<ArgumentException>(() => new ExpenceReportService(mockExpenceReport));
         }
     }
 }
