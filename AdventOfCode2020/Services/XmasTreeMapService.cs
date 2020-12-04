6eb23e26dd253396f42962e141d6edb2dbccf9b6
Chris Ashton <webdev@snerpton.net>
2020-12-04 02:43:31 +0000

    Able to correctly calculate example via unit test.

--- AdventOfCode2020/Services/XmasTreeMapService.cs
+++ AdventOfCode2020/Services/XmasTreeMapService.cs
@@ -1,25 +1,64 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode2020.Repositories;
 
 namespace AdventOfCode2020.Services
 {
     public class XmasTreeMapService
     {
-        // public static int MinMapWidth(int singleMoveX, int singleMoveY, int mapHeight)
-        // {
-        //     if (singleMoveX < 0)
-        //         throw new ArgumentOutOfRangeException(nameof(singleMoveX));
-        //     
-        //     if (singleMoveY < 0)
-        //         throw new ArgumentOutOfRangeException(nameof(singleMoveY));
-        //     
-        //     if (mapHeight < 1)
-        //         throw new ArgumentOutOfRangeException(nameof(mapHeight));
-        //
-        //     var numberOfVerticalMoves = mapHeight / singleMoveY + (mapHeight % singleMoveY > 0 ? 1 : 0);
-        //     var numberOfHorizontalMoves = numberOfVerticalMoves;
-        //     var minMapWidth = numberOfHorizontalMoves * singleMoveX;
-        //
-        //     return minMapWidth;
-        // }
+        private readonly IEnumerable<MapLocation> _xmasTreeMap;
+        private readonly int _singleMoveX;
+        private readonly int _singleMoveY;
+        
+        public XmasTreeMapService(IEnumerable<MapLocation> xmasTreeMap , int singleMoveX, int singleMoveY)
+        {
+            if (xmasTreeMap == null)
+                throw new ArgumentNullException(nameof(xmasTreeMap));
+
+            if (xmasTreeMap.Any() == false)
+                throw new ArgumentOutOfRangeException(nameof(xmasTreeMap));
+
+            _xmasTreeMap = xmasTreeMap;
+
+            if (singleMoveX < 0)
+                throw new ArgumentOutOfRangeException(nameof(singleMoveX));
+
+            _singleMoveX = singleMoveX;
+
+            if (singleMoveY < 0)
+                throw new ArgumentOutOfRangeException(nameof(singleMoveY));
+
+            _singleMoveY = singleMoveY;
+        }
+
+        public int CountTrees()
+        {
+            var currentPositionX = 0;
+            var currentPositionY = 0;
+            var maxIndexX = _xmasTreeMap.Max(x => x.XPosition);
+            var maxIndexY = _xmasTreeMap.Max(x => x.YPosition);
+            var treesEncountered = 0;
+
+            while (currentPositionY <= maxIndexY)
+            {
+                var newPositionX = currentPositionX + _singleMoveX > maxIndexX
+                    ? currentPositionX + _singleMoveX - maxIndexX -1
+                    : currentPositionX + _singleMoveX;
+                var newPositionY = currentPositionY + _singleMoveY;
+
+                currentPositionX = newPositionX;
+                currentPositionY = newPositionY;
+                
+                if (currentPositionY > maxIndexY)
+                    break;
+                
+                var newPosition =
+                    _xmasTreeMap.Single(l => l.XPosition == newPositionX && l.YPosition == newPositionY);
+                treesEncountered = newPosition.Type == PositionType.Tree ? treesEncountered + 1 : treesEncountered;
+            }
+
+            return treesEncountered;
+        }
     }
 }
--- AdventOfCode2020Tests/XmasTreeMapServiceTests.cs
+++ AdventOfCode2020Tests/XmasTreeMapServiceTests.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using AdventOfCode2020.Repositories;
 using AdventOfCode2020.Services;
 using NUnit.Framework;
 using NUnit.Framework.Internal;
@@ -7,36 +9,62 @@ namespace AdventOfCode2020Tests
 {
     public class XmasTreeMapServiceTests
     {
-        // public class MinMapWidthTests
-        // {
-        //     [Test]
-        //     [TestCase(-1,1)]
-        //     [TestCase(1,-1)]
-        //     public void Should_ThrowException_When_InvalidSingleMoves(int singleMoveX, int singleMoveY)
-        //     {
-        //         Assert.Throws<ArgumentOutOfRangeException>(() =>
-        //             XmasTreeMapService.MinMapWidth(singleMoveX, singleMoveY, 1));
-        //     }  
-        //     [Test]
-        //     [TestCase(0)]
-        //     [TestCase(-1)]
-        //     public void Should_ThrowException_When_InvalidMapHeight(int mapHeight)
-        //     {
-        //         Assert.Throws<ArgumentOutOfRangeException>(() =>
-        //             XmasTreeMapService.MinMapWidth(1, 1, mapHeight));
-        //     }
-        //
-        //     [Test]
-        //     [TestCase(1,1,1,2)]
-        //     [TestCase(1,1,2,3)]
-        //     [TestCase(1,1,3,4)]
-        //     [TestCase(3,1,11,31)] // From Day 3 documentation
-        //     public void Should_CalculateMapHeight_When_ValidSingleMoveAndMapHeight(int singleMoveX, int singleMoveY, int mapHeight, int expectedMinMapWidth)
-        //     {
-        //         var resultMapWidth = XmasTreeMapService.MinMapWidth(singleMoveX, singleMoveY, mapHeight);
-        //         Assert.That(resultMapWidth == expectedMinMapWidth, $"{resultMapWidth} != {expectedMinMapWidth}");
-        //     }
-        // }
+        private class XmasTreeMapRepositoryWrapper : XmasTreeMapRepository
+        {
+            public new IEnumerable<MapLocation> MapLocationsFromLine(string line, int yPosition) => base.MapLocationsFromLine(line, yPosition);
+            public new IEnumerable<MapLocation> MapLocationsFromLines(string[] lines) => base.MapLocationsFromLines(lines);
+        }
+        
+        [Test]
+        public void Should_ThrowException_When_NullXmasTreeMap()
+        {
+            Assert.Throws<ArgumentNullException>(() => new XmasTreeMapService(null, 1, 1));
+        }
+        
+        [Test]
+        public void Should_ThrowException_When_EmptyXmasTreeMap()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new XmasTreeMapService(new List<MapLocation>(), 1, 1));
+        }
+        
+        [Test]
+        [TestCase(-1,1)]
+        [TestCase(1,-1)]
+        public void Should_ThrowException_When_InvalidSingleMoves(int singleMoveX, int singleMoveY)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new XmasTreeMapService(new List<MapLocation>(), singleMoveX, singleMoveY));
+        }
 
+        public class CountTreesTests
+        {
+            [Test]
+            public void Should_CalculateNumberOfTrees()
+            {
+                var lines = new List<string>
+                {
+                    "..##.......",
+                    "#...#...#..",
+                    ".#....#..#.",
+                    "..#.#...#.#",
+                    ".#...##..#.",
+                    "..#.##.....",
+                    ".#.#.#....#",
+                    ".#........#",
+                    "#.##...#...",
+                    "#...##....#",
+                    ".#..#...#.#"
+                };
+
+                int singleMoveX = 3;
+                int singleMoveY = 1;
+                var expectedNumberOfTrees = 7;
+                var xmasTreeMap = new XmasTreeMapRepositoryWrapper();
+                var sut = new XmasTreeMapService(xmasTreeMap.MapLocationsFromLines(lines.ToArray()), singleMoveX, singleMoveY);
+                
+                var resultNumberOfTrees = sut.CountTrees();
+                
+                Assert.That(resultNumberOfTrees == expectedNumberOfTrees, $"{resultNumberOfTrees} : {expectedNumberOfTrees}");
+            }
+        }
     }
 }
