c03b1652c25d8ee1a292841fea8c3266c820a8e2
Chris Ashton <webdev@snerpton.net>
2020-12-04 23:49:53 +0000

    Write unit tests for validating a passport.

--- AdventOfCode2020.Day4/AdventOfCode2020.Day4.Tests/PassportExtensionsTests.cs
+++ AdventOfCode2020.Day4/AdventOfCode2020.Day4.Tests/PassportExtensionsTests.cs
@@ -12,5 +12,173 @@ namespace AdventOfCode2020.Day4.Tests
             
             Assert.Throws<ArgumentNullException>(() => sut.Validate());
         }
+  
+        [Test]
+        [TestCase("")]
+        [TestCase(" ")]
+        [TestCase(null)]
+        public void Should_ReturnFalse_When_InvalidBirthYear(string birthYear)
+        {
+            var sut = new Passport
+            {
+                BirthYear = birthYear,
+                CountryId = "aaa",
+                ExpirationYear = "aaa",
+                EyeColor = "aaa",
+                Height = "aaa",
+                HairColor = "aaa",
+                IssueYear = "aaa",
+                PassportId = "aaa"
+            };
+            
+            Assert.That(sut.Validate() == false);
+        }
+        
+        [Test]
+        [TestCase("")]
+        [TestCase(" ")]
+        [TestCase(null)]
+        public void Should_ReturnFalse_When_InvalidCountryId(string countryId)
+        {
+            var sut = new Passport
+            {
+                BirthYear = "aaa",
+                CountryId = countryId,
+                ExpirationYear = "aaa",
+                EyeColor = "aaa",
+                Height = "aaa",
+                HairColor = "aaa",
+                IssueYear = "aaa",
+                PassportId = "aaa"
+            };
+            
+            Assert.That(sut.Validate() == false);
+        }
+        
+        [Test]
+        [TestCase("")]
+        [TestCase(" ")]
+        [TestCase(null)]
+        public void Should_ReturnFalse_When_InvalidExpirationYear(string expirationYear)
+        {
+            var sut = new Passport
+            {
+                BirthYear = "aaa",
+                CountryId = "aaa",
+                ExpirationYear = expirationYear,
+                EyeColor = "aaa",
+                Height = "aaa",
+                HairColor = "aaa",
+                IssueYear = "aaa",
+                PassportId = "aaa"
+            };
+            
+            Assert.That(sut.Validate() == false);
+        }
+
+        [Test]
+        [TestCase("")]
+        [TestCase(" ")]
+        [TestCase(null)]
+        public void Should_ReturnFalse_When_InvalidEyeColor(string eyeColor)
+        {
+            var sut = new Passport
+            {
+                BirthYear = "aaa",
+                CountryId = "aaa",
+                ExpirationYear = "2050",
+                EyeColor = eyeColor,
+                Height = "aaa",
+                HairColor = "aaa",
+                IssueYear = "aaa",
+                PassportId = "aaa"
+            };
+            
+            Assert.That(sut.Validate() == false);
+        }
+        
+        [Test]
+        [TestCase("")]
+        [TestCase(" ")]
+        [TestCase(null)]
+        public void Should_ReturnFalse_When_InvalidHeight(string height)
+        {
+            var sut = new Passport
+            {
+                BirthYear = "aaa",
+                CountryId = "aaa",
+                ExpirationYear = "2050",
+                EyeColor = "aaa",
+                Height = height,
+                HairColor = "aaa",
+                IssueYear = "aaa",
+                PassportId = "aaa"
+            };
+            
+            Assert.That(sut.Validate() == false);
+        }
+        
+        [Test]
+        [TestCase("")]
+        [TestCase(" ")]
+        [TestCase(null)]
+        public void Should_ReturnFalse_When_InvalidHairColor(string hairColor)
+        {
+            var sut = new Passport
+            {
+                BirthYear = "aaa",
+                CountryId = "aaa",
+                ExpirationYear = "2050",
+                EyeColor = "aaa",
+                Height = "aaa",
+                HairColor = hairColor,
+                IssueYear = "aaa",
+                PassportId = "aaa"
+            };
+            
+            Assert.That(sut.Validate() == false);
+        }
+        
+        [Test]
+        [TestCase("")]
+        [TestCase(" ")]
+        [TestCase(null)]
+        public void Should_ReturnFalse_When_InvalidIssueYear(string issueYear)
+        {
+            var sut = new Passport
+            {
+                BirthYear = "aaa",
+                CountryId = "aaa",
+                ExpirationYear = "2050",
+                EyeColor = "aaa",
+                Height = "aaa",
+                HairColor = "aaa",
+                IssueYear = issueYear,
+                PassportId = "aaa"
+            };
+            
+            Assert.That(sut.Validate() == false);
+        }
+        
+        [Test]
+        [TestCase("")]
+        [TestCase(" ")]
+        [TestCase(null)]
+        public void Should_ReturnFalse_When_InvalidPassportId(string passportId)
+        {
+            var sut = new Passport
+            {
+                BirthYear = "aaa",
+                CountryId = "aaa",
+                ExpirationYear = "2050",
+                EyeColor = "aaa",
+                Height = "aaa",
+                HairColor = "aaa",
+                IssueYear = "aaa",
+                PassportId = passportId
+            };
+            
+            Assert.That(sut.Validate() == false);
+        }
     }
 }
--- AdventOfCode2020.Day4/AdventOfCode2020.Day4/Passport.cs
+++ AdventOfCode2020.Day4/AdventOfCode2020.Day4/Passport.cs
@@ -2,13 +2,13 @@ namespace AdventOfCode2020.Day4
 {
     public class Passport
     {
-        public int BirthYear { get; set; }
-        public int IssueYear { get; set; }
-        public int ExpirationYear { get; set; }
+        public string BirthYear { get; set; }
+        public string CountryId { get; set; }
+        public string ExpirationYear { get; set; }
+        public string EyeColor { get; set; }
         public string Height { get; set; }
         public string HairColor { get; set; }
-        public string EyeColor { get; set; }
-        public int PassportId { get; set; }
-        public int CountryId { get; set; }
+        public string IssueYear { get; set; }
+        public string PassportId { get; set; }
     }
 }
