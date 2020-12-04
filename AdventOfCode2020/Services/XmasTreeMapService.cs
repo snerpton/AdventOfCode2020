2d5d2977d9779f4ad982c0cbe3cbda3ead3c7488
Chris Ashton <webdev@snerpton.net>
2020-12-04 01:11:58 +0000

    Comment code... was working on repeating the map, but this is the same as appearing on the left of the map when disappearing of the right.

---
+++ AdventOfCode2020/Services/XmasTreeMapService.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace AdventOfCode2020.Services
+{
+    public class XmasTreeMapService
+    {
+        // public static int MinMapWidth(int singleMoveX, int singleMoveY, int mapHeight)
+        // {
+        //     if (singleMoveX < 0)
+        //         throw new ArgumentOutOfRangeException(nameof(singleMoveX));
+        //     
+        //     if (singleMoveY < 0)
+        //         throw new ArgumentOutOfRangeException(nameof(singleMoveY));
+        //     
+        //     if (mapHeight < 1)
+        //         throw new ArgumentOutOfRangeException(nameof(mapHeight));
+        //
+        //     var numberOfVerticalMoves = mapHeight / singleMoveY + (mapHeight % singleMoveY > 0 ? 1 : 0);
+        //     var numberOfHorizontalMoves = numberOfVerticalMoves;
+        //     var minMapWidth = numberOfHorizontalMoves * singleMoveX;
+        //
+        //     return minMapWidth;
+        // }
+    }
+}
---
+++ AdventOfCode2020Tests/XmasTreeMapServiceTests.cs
@@ -0,0 +1,42 @@
+using System;
+using AdventOfCode2020.Services;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace AdventOfCode2020Tests
+{
+    public class XmasTreeMapServiceTests
+    {
+        // public class MinMapWidthTests
+        // {
+        //     [Test]
+        //     [TestCase(-1,1)]
+        //     [TestCase(1,-1)]
+        //     public void Should_ThrowException_When_InvalidSingleMoves(int singleMoveX, int singleMoveY)
+        //     {
+        //         Assert.Throws<ArgumentOutOfRangeException>(() =>
+        //             XmasTreeMapService.MinMapWidth(singleMoveX, singleMoveY, 1));
+        //     }  
+        //     [Test]
+        //     [TestCase(0)]
+        //     [TestCase(-1)]
+        //     public void Should_ThrowException_When_InvalidMapHeight(int mapHeight)
+        //     {
+        //         Assert.Throws<ArgumentOutOfRangeException>(() =>
+        //             XmasTreeMapService.MinMapWidth(1, 1, mapHeight));
+        //     }
+        //
+        //     [Test]
+        //     [TestCase(1,1,1,2)]
+        //     [TestCase(1,1,2,3)]
+        //     [TestCase(1,1,3,4)]
+        //     [TestCase(3,1,11,31)] // From Day 3 documentation
+        //     public void Should_CalculateMapHeight_When_ValidSingleMoveAndMapHeight(int singleMoveX, int singleMoveY, int mapHeight, int expectedMinMapWidth)
+        //     {
+        //         var resultMapWidth = XmasTreeMapService.MinMapWidth(singleMoveX, singleMoveY, mapHeight);
+        //         Assert.That(resultMapWidth == expectedMinMapWidth, $"{resultMapWidth} != {expectedMinMapWidth}");
+        //     }
+        // }
+
+    }
+}
