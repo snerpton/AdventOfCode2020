2b64371007af6cee27ad131ac2031eca1f8e4dcf
Chris Ashton <webdev@snerpton.net>
2020-12-05 22:45:47 +0000

    Day 5 part 1 answer: code to calculate highest seat ID
    Highest seat ID = 832

--- AdventOfCode.Day5/AdventOfCode.Day5/Program.cs
+++ AdventOfCode.Day5/AdventOfCode.Day5/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 
 namespace AdventOfCode.Day5
 {
@@ -6,7 +9,20 @@ namespace AdventOfCode.Day5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Advent of Code 2020, Day 5!");
+            
+            var boardingPassFileAndPath = Path.GetDirectoryName(new Uri(Assembly.GetExecutingAssembly().Location).LocalPath) +
+                               "/Assets/Day5PuzzleInputBoardingPass.txt";
+
+            var boardingPassRows = File.ReadLines(boardingPassFileAndPath);
+            var boardingPass = boardingPassRows.Select(r => new BoardingPass(r));
+            
+            Console.WriteLine($"Number of entries in Boarding Pass input file: {boardingPassRows.Count()}");
+            Console.WriteLine($"Number of valid Boarding Pass: {boardingPass.Count(x => x.IsValid())}");
+
+            var highestSeatId = boardingPass.Max(x => x.SeatId());
+            
+            Console.WriteLine($"Highest  Seat ID: {highestSeatId}");
         }
     }
 }
