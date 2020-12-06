1c6ab67708a98b66783d70dfe223f279b0e28226
Chris Ashton <webdev@snerpton.net>
2020-12-06 21:51:10 +0000

    Populate and validate individual responses.

--- AdventOfCode2020.Day6/AdventOfCode2020.Day6.Tests/IndividualsResponseTests.cs
+++ AdventOfCode2020.Day6/AdventOfCode2020.Day6.Tests/IndividualsResponseTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using NUnit.Framework;
 
 namespace AdventOfCode2020.Day6.Tests
@@ -6,9 +7,44 @@ namespace AdventOfCode2020.Day6.Tests
     public class IndividualsResponseTests
     {
         [Test]
-        public void Should_ThrowException_When_NullYesTo()
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase("   ")]
+        public void Should_ThrowException_When_NullOrEmptyYesTo(string yesNo)
         {
-            Assert.Throws<ArgumentNullException>(() => new IndividualsResponse(null));
+            Assert.Throws<ArgumentNullException>(() => new IndividualsResponse(yesNo));
+        }
+    
+        [Test]
+        [TestCase("ABA")]
+        [TestCase("AA")]
+        public void Should_ThrowException_When_DuplicateYesTo(string yesTo)
+        {
+            Assert.Throws<ArgumentException>(() => new IndividualsResponse(yesTo));
+        }
+        
+        [Test]
+        public void Should_ThrowException_When_LowercaseYesTo()
+        {
+            foreach(var letter in "abcdefghijklmnopqrstuvwxyz")
+                Assert.Throws<ArgumentException>(() => new IndividualsResponse(letter.ToString()));
+        }
+        
+        [Test]
+        [TestCase("ABC", new object[]{'A','B','C'})]
+        [TestCase("ACB", new object[]{'A','B','C'})]
+        [TestCase("ABCXYZ", new object[]{'A','B','C','X','Y','Z'})]
+        public void Should_PopulateYesTo_When_ValidYesTo(string yesTo, object[] expectedYesToObj)
+        {
+            var expectedYesTo = expectedYesToObj.Select(x => x.ToString()[0]).ToArray();
+            var sut = new IndividualsResponse(yesTo);
+            
+            
+            Assert.That(sut.YesTo.Length == yesTo.Length, $"input ({sut.YesTo.Length}) and output ({yesTo.Length}) different lengths.");
+            for (var index=0; index<sut.YesTo.Length; index++)
+            {
+                Assert.That(sut.YesTo[index] == expectedYesTo[index], $"sut YesTo ({sut.YesTo[index]}) does not equal expected yesTo ({expectedYesTo[index]}) ");
+            }
         }
     }
 }
--- AdventOfCode2020.Day6/AdventOfCode2020.Day6/IndividualsResponse.cs
+++ AdventOfCode2020.Day6/AdventOfCode2020.Day6/IndividualsResponse.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 
 namespace AdventOfCode2020.Day6
 {
@@ -10,8 +11,21 @@ namespace AdventOfCode2020.Day6
         {
             if (string.IsNullOrWhiteSpace(yesTo))
                 throw new ArgumentNullException();
+
+            if (ValidateAsUppercase(yesTo) == false)
+                throw new ArgumentException(nameof(yesTo));
             
-            throw new NotImplementedException();
+            if (ValidateNoDuplicates(yesTo) == false)
+                throw new ArgumentException(nameof(yesTo));
+            
+            var yesToCharacters = yesTo.Select(x => x);
+
+            YesTo = yesToCharacters.OrderBy(x => x).ToArray();
         }
+
+        private bool ValidateAsUppercase(string letters) =>
+            !letters.Any(l => "abcdefghijklmnopqrstuvwxyz".Contains(l));
+
+        private bool ValidateNoDuplicates(string letters) => letters.Distinct().Count() == letters.Length;
     }
 }
