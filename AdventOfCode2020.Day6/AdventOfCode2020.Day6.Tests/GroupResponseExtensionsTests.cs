462dfdc8a8350ec6c5e94b1358aabda689730cb2
Chris Ashton <webdev@snerpton.net>
2020-12-06 22:34:28 +0000

    Refactor groupResponseExtensionTests in preparation for sum calculation tests.

--- AdventOfCode2020.Day6/AdventOfCode2020.Day6.Tests/GroupResponseExtensionsTests.cs
+++ AdventOfCode2020.Day6/AdventOfCode2020.Day6.Tests/GroupResponseExtensionsTests.cs
@@ -6,40 +6,44 @@ namespace AdventOfCode2020.Day6.Tests
 {
     public class GroupResponseExtensionsTests
     {
-        [Test]
-        public void Should_ThrowException_When_NullGroupResponse()
+        public class QuestionsAnsweredWithYesTests
         {
-            GroupResponse sut = null;
-
-            Assert.Throws<ArgumentNullException>(() => sut.QuestionsAnsweredWithYes());
-        }
-
-        [Test]
-        public void Should_ReturnEmptyCollection_When_GroupResponseIndividualsResponsesNotPopulated()
-        {
-            var sut = new GroupResponse();
-            
-            Assert.That(sut.QuestionsAnsweredWithYes().Length == 0);
-        }
-        
-        [Test]
-        [TestCase(new object[]{"acbx", "abcy"}, "abcxy")]
-        [TestCase(new object[]{"abc"}, "abc")]
-        [TestCase(new object[]{"a", "b", "c"}, "abc")]
-        [TestCase(new object[]{"ab", "ac"}, "abc")]
-        [TestCase(new object[]{"a", "a", "a", "a"}, "a")]
-        [TestCase(new object[]{"b"}, "b")]
-        public void Should_DetermineQuestionsAnsweredYesTo_When_ValidGroupResponse(object[] groupResponseObj, string expectedYesTo)
-        {
-            var groupResponse = groupResponseObj.Select(x => x.ToString());
-            
-            var sut = new GroupResponse
+            [Test]
+            public void Should_ThrowException_When_NullGroupResponse()
             {
-                IndividualsResponses = groupResponse.Select(x => new IndividualsResponse(x))
-            };
+                GroupResponse sut = null;
+
+                Assert.Throws<ArgumentNullException>(() => sut.QuestionsAnsweredWithYes());
+            }
+
+            [Test]
+            public void Should_ReturnEmptyCollection_When_GroupResponseIndividualsResponsesNotPopulated()
+            {
+                var sut = new GroupResponse();
+            
+                Assert.That(sut.QuestionsAnsweredWithYes().Length == 0);
+            }
         
-            Assert.That(sut.QuestionsAnsweredWithYes() == expectedYesTo,
-                $"Actual ({sut.QuestionsAnsweredWithYes()}) not equal to expected ({expectedYesTo}).");
+            [Test]
+            [TestCase(new object[]{"acbx", "abcy"}, "abcxy")]
+            [TestCase(new object[]{"abc"}, "abc")]
+            [TestCase(new object[]{"a", "b", "c"}, "abc")]
+            [TestCase(new object[]{"ab", "ac"}, "abc")]
+            [TestCase(new object[]{"a", "a", "a", "a"}, "a")]
+            [TestCase(new object[]{"b"}, "b")]
+            public void Should_DetermineQuestionsAnsweredYesTo_When_ValidGroupResponse(object[] groupResponseObj, string expectedYesTo)
+            {
+                var groupResponse = groupResponseObj.Select(x => x.ToString());
+            
+                var sut = new GroupResponse
+                {
+                    IndividualsResponses = groupResponse.Select(x => new IndividualsResponse(x))
+                };
+        
+                Assert.That(sut.QuestionsAnsweredWithYes() == expectedYesTo,
+                    $"Actual ({sut.QuestionsAnsweredWithYes()}) not equal to expected ({expectedYesTo}).");
+            }
         }
+        
     }
 }
