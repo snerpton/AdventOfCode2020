32dba1bb5e875eb5b1b2d12a770acd6665b2716e
Chris Ashton <webdev@snerpton.net>
2020-12-10 21:34:20 +0000

    Solution to Day 7 part 2.

--- AdventOfCode2020.Day7/AdventOfCode2020.Day7/Program.cs
+++ AdventOfCode2020.Day7/AdventOfCode2020.Day7/Program.cs
@@ -15,8 +15,13 @@ namespace AdventOfCode2020.Day7
             var bagColour = "shiny gold";
             var matchingBags = bagService.FindNumberOfBagsThatEventuallyContain(bagColour);
 
+            Console.WriteLine();
+            Console.WriteLine("Part 1...");
             Console.WriteLine($"Number of bags that eventually contain '{bagColour}': {matchingBags}");
 
+            Console.WriteLine();
+            Console.WriteLine("Part2...");
+            Console.WriteLine($"Number of individual bags required to fit inside single 'shiny gold' bag: {bagService.TotalNumberOfBagsContainedIn("shiny gold")}");
         }
     }
 }
