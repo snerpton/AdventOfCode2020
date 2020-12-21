561456e7e92a5ea9e1bd587ff36f99c5a26ec720
Chris Ashton <webdev@snerpton.net>
2020-12-21 23:11:14 +0000

    Validate input and if it false return the first number using provided sample for unit test.

--- AdventOfCode2020.Day9/AdventOfCode2020.Day9.Tests/XmasCypherHelpersTests.cs
+++ AdventOfCode2020.Day9/AdventOfCode2020.Day9.Tests/XmasCypherHelpersTests.cs
@@ -27,12 +27,51 @@ namespace AdventOfCode2020.Day9.Tests
         [TestCase(0)]
         [TestCase(-1)]
         [TestCase(-10)]
-        public void Should_ThrowException_When_ZeroOrNagativePreamble(int preamble)
+        public void Should_ThrowException_When_ZeroOrNegativePreamble(int preamble)
         {
             var inputLines = new long[] {1, 2, 3};
             var sut = new XmasCypherHelpers(inputLines);
 
             Assert.Throws<ArgumentOutOfRangeException>(() => sut.Validate(preamble, out var firstInvalidNumber));
+        }
+
+        [Test]
+        public void Should_Validate_When_ValidInput()
+        {
+            var inputLines = new long[] {35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182};
+            var preamble = 5;
+            var sut = new XmasCypherHelpers(inputLines);
+            
+            Assert.That(sut.Validate(preamble, out var firstInvalidNumber), Is.EqualTo(true));
+        }
+        
+        [Test]
+        public void Should_NotValidate_When_InvalidInput()
+        {
+            var inputLines = new long[]
+            {
+                35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182, 127, 219, 299, 277, 309, 576
+            };
+            var preamble = 5;
+            var sut = new XmasCypherHelpers(inputLines);
+            
+            Assert.That(sut.Validate(preamble, out var firstInvalidNumber), Is.EqualTo(false));
+        }
+        
+        [Test]
+        public void Should_FindFirstInvalidNumber_When_InvalidInput()
+        {
+            var inputLines = new long[]
+            {
+                35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182, 127, 219, 299, 277, 309, 576
+            };
+            var preamble = 5;
+            var sut = new XmasCypherHelpers(inputLines);
+
+            sut.Validate(preamble, out var firstInvalidNumber);
+            var expectedFirstInvalidNumber = 127;
+
+            Assert.That(firstInvalidNumber == expectedFirstInvalidNumber);
         }
     }
 }
--- AdventOfCode2020.Day9/AdventOfCode2020.Day9/XmasCypherHelpers.cs
+++ AdventOfCode2020.Day9/AdventOfCode2020.Day9/XmasCypherHelpers.cs
@@ -6,7 +6,7 @@ namespace AdventOfCode2020.Day9
 {
     public class XmasCypherHelpers
     {
-        private readonly IEnumerable<long> _inputLines;
+        private readonly List<long> _inputLines;
 
         public XmasCypherHelpers(IEnumerable<long> inputLines)
         {
@@ -16,15 +16,49 @@ namespace AdventOfCode2020.Day9
             if (inputLines.Any() == false)
                 throw new ArgumentOutOfRangeException(nameof(inputLines));
 
-            _inputLines = inputLines;
+            _inputLines = inputLines.ToList();
         }
 
         public bool Validate(int preambleCount, out long firstInvalidNumber)
         {
             if (preambleCount <= 0)
                 throw new ArgumentOutOfRangeException(nameof(firstInvalidNumber));
+
+            firstInvalidNumber = long.MinValue;
+
+            var currentPreambleIndexMin = 0;
+          
+            foreach (var input in _inputLines.GetRange(preambleCount, _inputLines.Count - preambleCount))
+            {
+                var currentPreamble = CurrentPreamble(currentPreambleIndexMin, preambleCount);
+
+                if (CanFindSumInPreamble(input, currentPreamble) == false)
+                {
+                    firstInvalidNumber = input;
+                    return false;
+                }
+                
+                currentPreambleIndexMin++;
+            }
             
-            throw new NotImplementedException();
+            return true;
         }
+
+        private static bool CanFindSumInPreamble(long currentNumber, List<long> preamble)
+        {
+            for (var i = 0; i < preamble.Count - 1; i++)
+            {
+                for (var j = i + 1; j < preamble.Count - 1; j++)
+                {
+                    if (preamble[i] + preamble[j] == currentNumber)
+                        return true;
+                }
+            }
+
+            return false;
+        }
+
+        private List<long> CurrentPreamble(int rangeIndexMin, int count) =>
+            _inputLines.GetRange(rangeIndexMin, count);
     }
 }
