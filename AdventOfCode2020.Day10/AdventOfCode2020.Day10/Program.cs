c52d4f7fbf367daa954312d3a10ebfd529d1ff80
Chris Ashton <webdev@snerpton.net>
2021-01-01 21:56:39 +0000

    Day 10 part 1 solution.

--- AdventOfCode2020.Day10/AdventOfCode2020.Day10/Program.cs
+++ AdventOfCode2020.Day10/AdventOfCode2020.Day10/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.IO;
 using System.Reflection;
 
@@ -14,7 +15,16 @@ namespace AdventOfCode2020.Day10
                                   "/Assets/Day10PuzzleInputOutputJoltage.txt";
 
             var fileContentOfAdapterJoltages = FileParser.Read(filePathAndName);
+            var adapters = FileParser.Parse(fileContentOfAdapterJoltages);
 
+            var joltageAdapterService = new JoltageAdapterService(adapters);
+            var numberWith1JoltStep = joltageAdapterService.NumberWithJoltDifferenceOf(1);
+            Console.WriteLine($"Number with 1 Jolt difference: {numberWith1JoltStep}");
+            
+            var numberWith3JoltStep = joltageAdapterService.NumberWithJoltDifferenceOf(3);
+            Console.WriteLine($"Number with 1 Jolt difference: {numberWith3JoltStep}");
+
+            Console.WriteLine($"Number of 1-jolt differences multiplied by the number of 3-jolt differences: {numberWith1JoltStep * numberWith3JoltStep}");
         }
     }
 }
